5071088401dfefbc05d5e33d27b16bd8eaa1abf1
Romain Fournier <romain.fournier26@gmail.com>
2018-03-18 17:47:44 +0100

    need to pull

--- positif/src/main/java/service/MainEmploye.java
+++ positif/src/main/java/service/MainEmploye.java
@@ -13,26 +13,32 @@ import om.Employe;
  */
 public class MainEmploye {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("bienvenue sur positif, service Employe");
-        boolean identified = false;
-        String id = "";
-        String mdp="";
-        Employe e=null;
-        while(identified == false){
-            System.out.println("id ?");
-            id = sc.nextLine();
-            System.out.println("mdp ?");
-            mdp = sc.nextLine();
-            e=Service.identificationEmploye(id,mdp);
-            if(e==null){
-                System.out.println("nom de compte ou mot de passe incorrect, réessayez");
-            }else{
-                identified = true;
+        while(true){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("bienvenue sur positif, terminal Employe");
+            System.out.println("1 - connexion");
+            System.out.println("2 - deconnexion");
+            boolean identified = false;
+            String id = "";
+            String mdp="";
+            Employe e=null;
+            while(identified == false){
+                System.out.println("id ?");
+                id = sc.nextLine();
+                System.out.println("mdp ?");
+                mdp = sc.nextLine();
+                e=Service.identificationEmploye(id,mdp);
+                if(e==null){
+                    System.out.println("nom de compte ou mot de passe incorrect, réessayez");
+                }else{
+                    identified = true;
+                }
             }
-        }
-        System.out.println("identifié en tant que"+e.getPrenom()+" "+e.getNom());
-            
+            while(true){
+                System.out.println("identifié en tant que"+e.getPrenom()+" "+e.getNom());
+                System.out.println("1 - onglet voyance");
+                System.out.println("2 - onglet stats");
+                System.out.println("3 - deconnexion");
     }
 
 }
